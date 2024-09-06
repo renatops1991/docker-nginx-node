12202efe38cfa1e9b76116a18de7654e66e7be5c
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-06 12:06:20 -0300

    refactor: ensure show people list from dabatabase
    Signed-off-by: Renato Pereira Santos <renatops1991@gmail.com>

--- node/app/index.js
+++ node/app/index.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const port = 3000;
 const app = express();
 
+const connection = mysql.createConnection({
+    host: "mysqlDb",
+    user: "root",
+    password: "root",
+    database: "full-cycle-db"
+});
+
+const query = 'INSERT INTO people (name, email) VALUES ("Full Cycle", "fullcycle@gmail.com")'
+connection.query(query)
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!!</h1>');
+    connection.query('SELECT * FROM people', (err, results) => {
+        const people = results.map(person => ({
+                name: person.name,
+                email: person.email
+        }))
+
+        const fullCycleCompleteBody =`
+            <h1>Full Cycle</h1>
+            <h2>People</h2>
+            <ul>
+                ${people.map(person => `<li>${person.name} - ${person.email}</li>`).join('')}
+            </ul>`
+        connection.end();
+        res.send(fullCycleCompleteBody);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`listening on port ${port}!`);
 });
